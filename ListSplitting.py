64d30ef00d1961a10bfb914c4518866218999953
SrinivasanAgile <srinivasan.vcs@gmail.com>
2022-01-29 22:56:24 +0530

    Test

---
+++ ListSplitting.py
@@ -0,0 +1,32 @@
+"""Addition = (input("Enter the values that needs to be added: \n"))
+abc = Addition.split(",")
+print(len(abc))
+print(abc)"""
+total = 0
+'''for x in range(0, len(a)):
+    total = total + a[x]'''
+ele = 0
+
+abc = [2,2,1]
+print(abc[1])
+while (ele < len(abc)):
+    total = total + abc[ele]
+    ele +=1
+print(total)
+"""
+    # Python program to find sum of elements in list
+total = 0
+ele = 0
+
+    # creating a list
+list1 = [11, 5, 17, 18, 23]
+
+    # Iterate each element in list
+    # and add them in variable total
+while (ele < len(list1)):
+        total = total + list1[ele]
+        ele += 1
+
+    # printing total value
+print("Sum of all elements in given list: ", total)
+"""
