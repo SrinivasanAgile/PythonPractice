c444b174113cd66a854af0c78472101c3e30a53b
SrinivasanAgile <srinivasan.vcs@gmail.com>
2022-01-29 22:47:09 +0530

    Test

---
+++ main.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def print_start(start):
+        print(start)
+
+def print_twice(message):
+    print(2*message)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('hello')
+print_start("start to learn this program")
+print_start("This is my first program")
+print_twice("this will be printed twice")
+
+
+x=10
+y=10.25
+z=3j
+
+a='string as ABC'
+print(x)
+print(y)
+print(z)
+print(a)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+
+print(id(x))
+print(id(y))
+print(id(z))
+print(id(a))
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
